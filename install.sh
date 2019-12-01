c4f2e9a4ec446f11965e87f1aa24ba6828c42baa
lintao <lintao209@outlook.com>
2019-12-01 19:50:40 +0800

    update && add pdf

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@
 # ros-kinetic-hector-gazebo-plugins \\ -y
 
 
-sudo apt-get install -y ros-kinetic-robot-localization \ 
-                        ros-kinetic-move-base \ 
-                        ros-kinetic-interactive-marker-twist-server \
-                        ros-kinetic-joint-state-controller \
-                        ros-kinetic-diff-drive-controller \
-                        ros-kinetic-lms1xx \ 
+sudo apt-get install -y ros-kinetic-robot-localization
+sudo apt-get install -y ros-kinetic-move-base
+sudo apt-get install -y ros-kinetic-interactive-marker-twist-server
+sudo apt-get install -y ros-kinetic-joint-state-controller
+sudo apt-get install -y ros-kinetic-diff-drive-controller
+sudo apt-get install -y ros-kinetic-lms1xx 
 
